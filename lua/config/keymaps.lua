47b5f85a615d0620c3d2852e479a847b71ba3481
royston allfrey <evariste@hotmail.co.uk>
2026-01-19 08:12:50 +0000

    key map for moving line up or down in nvim addded

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,13 @@ local map = vim.keymap.set
 map("n", "<leader>h", "<C-w>h", { desc = "Move back to filetree" })
 
 map("n", "<leader>l", "<C-w>l", { desc = "Move back to buffer" })
+
+-- Source - https://stackoverflow.com/a
+-- Posted by Brotify Force, modified by community. See post 'Timeline' for change history
+-- Retrieved 2026-01-07, License - CC BY-SA 4.0
+
+map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+
+map("n", "<C-j>", "<Esc>:move +1<CR>==", { desc = "Move line DOWN" })
+map("n", "<C-k>", "<Esc>:move -2<CR>==", { desc = "Move line UP" })
